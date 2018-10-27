9142a4edb8e8e76294b629d2feff863d112123a4
NaveenMagatala <naveenmagatala@gmail.com>
2018-10-26 22:17:11 -0400

    remeoved some variables from network calls which we might never use

--- BikeBuddy/Model/Network.swift
+++ BikeBuddy/Model/Network.swift
@@ -15,32 +15,21 @@ struct APIResponse: Decodable {
 
 class Network: Object, Decodable {
     
-    var company: List<String>?
-    @objc dynamic var href: String = ""
     @objc dynamic var location: Location?
     @objc dynamic var name: String = ""
-    @objc dynamic var id: String = ""
     
     @objc dynamic var uniqueKey: String = ""
     
     private enum CodingKeys: String, CodingKey {
-        case company, href, location, name, id
+        case location, name
     }
     
     convenience required init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        let href = try container.decode(String.self, forKey: .href)
         let name = try container.decode(String.self, forKey: .name)
-        let id = try container.decode(String.self, forKey: .name)
         let location = try container.decode(Location?.self, forKey: .location)
-        let companies = try container.decode([String]?.self, forKey: .company)
-        let realmCompanies = List<String>()
-        if let companies = companies {
-            realmCompanies.append(objectsIn: companies)
-        }
         let uniqueKey = "\(name)\(location?.latitude ?? 0)"
-        self.init(company: realmCompanies, href: href,
-                  location: location, name: name, id: id, uniqueKey: uniqueKey)
+        self.init(location: location, name: name, uniqueKey: uniqueKey)
     }
     
     override class func primaryKey() -> String? {
@@ -50,14 +39,10 @@ class Network: Object, Decodable {
 
 // Initializers
 extension Network {
-    convenience init(company: List<String>, href: String,
-                     location: Location?, name: String, id: String, uniqueKey: String) {
+    convenience init(location: Location?, name: String, uniqueKey: String) {
         self.init()
-        self.company = company
-        self.href = href
         self.location = location
         self.name = name
-        self.id = id
         self.uniqueKey = uniqueKey
     }
 }
